cbf760d534a576babe39a4befa70f915c17cf6fd
Mike Pall <mike>
2023-08-29 02:21:51 +0200

    FFI/ARM64/OSX: Handle non-standard OSX C calling conventions.
    Contributed by Peter Cawley.
    
    (cherry picked from commit 83954100dba9fc0cf5eeaf122f007df35ec9a604)
    
    This patch adds FFI support for passing small (< 8 bytes) parameters on
    the stack on the OSX arm64 architecture. Also, it fixes the compilation
    of FFI vararg functions, since before the patch arguments were passed in
    registers instead of the stack [1] for them. JIT machinery now uses
    `TREF_NIL` as a marker for the slot from which the variadic arguments
    begin.
    
    [1]: https://developer.apple.com/documentation/xcode/writing-arm64-code-for-apple-platforms#Update-code-that-passes-arguments-to-variadic-functions
    
    Sergey Kaplun:
    * added the description and the test for the problem
    
    Resolves tarantool/tarantool#6097
    Part of tarantool/tarantool#12480
    
    Reviewed-by: Evgeniy Temirgaleev <e.temirgaleev@tarantool.org>
    Reviewed-by: Sergey Bronnikov <sergeyb@tarantool.org>
    Signed-off-by: Sergey Kaplun <skaplun@tarantool.org>
    (cherry picked from commit 4150d948d9df878564911deef99109e230223b5c)

--- test/tarantool-tests/ffi-ccall/libfficcall.c
+++ test/tarantool-tests/ffi-ccall/libfficcall.c
@@ -1,3 +1,5 @@
+#include <stdint.h>
+
 struct sz12_t {
 	float f1;
 	float f2;
@@ -25,4 +27,52 @@ struct sz12_t sum3sz12(struct sz12_t a, struct sz12_t b, struct sz12_t c)
 	res.f2 = a.f2 + b.f2 + c.f2;
 	res.f3 = a.f3 + b.f3 + c.f3;
 	return res;
+}
+
+/****************************************************************/
+/*                           Enums.                             */
+/****************************************************************/
+
+typedef enum {
+	E1 = 1,
+	E2 = 2,
+	E3 = 3,
+	E4 = 4,
+	E5 = 5,
+	E6 = 6,
+	E7 = 7,
+	E8 = 8,
+	E9 = 9,
+	E10 = 10,
+	E11 = 11
+} enum_t;
+
+int test_enum_reg(enum_t e1, enum_t e2, enum_t e3)
+{
+	return e1 + e2 + e3;
+}
+
+int test_enum_stack(enum_t e1, enum_t e2, enum_t e3, enum_t e4, enum_t e5,
+		    enum_t e6, enum_t e7, enum_t e8, enum_t e9, enum_t e10,
+		    enum_t e11)
+{
+	return e1 + e2 + e3 + e4 + e5 + e6 + e7 + e8 + e9 + e10 + e11;
+}
+
+/****************************************************************/
+/*                  Basic types (< 8 bytes).                    */
+/****************************************************************/
+
+uint8_t test_u8_stack(uint8_t u1, uint8_t u2, uint8_t u3, uint8_t u4,
+		      uint8_t u5, uint8_t u6, uint8_t u7, uint8_t u8,
+		      uint8_t u9, uint8_t u10, uint8_t u11)
+{
+	return u1 + u2 + u3 + u4 + u5 + u6 + u7 + u8 + u9 + u10 + u11;
+}
+
+float test_float_stack(float f1, float f2, float f3, float f4, float f5,
+		       float f6, float f7, float f8, float f9, float f10,
+		       float f11)
+{
+	return f1 + f2 + f3 + f4 + f5 + f6 + f7 + f8 + f9 + f10 + f11;
 }
