2855eb9bb27f5d10e0439ba92cde550b24075cb6
Sergey Bronnikov <estetus@gmail.com>
2024-02-28 18:11:09 +0300

    tests: WIP test

--- test/tarantool-c-tests/lj-1087-vm-handler-call.test.c
+++ test/tarantool-c-tests/lj-1087-vm-handler-call.test.c
@@ -38,29 +38,125 @@ static int trace_cb(lua_State *L) {
 	return 0;
 }
 
-static int call_vm_handler(void *test_state)
+static int handle_luafunc_frame(void *test_state)
+{
+	/* Setup. */
+	lua_State *L = test_state;
+	jit_attach(L, (void *)trace_cb, "trace");
+
+	/* Loading and executing of a broken Lua code. */
+	luaL_dostring(L, "repeat until nil > 1");
+
+	/* Generates a Lua frame. */
+	luaL_dostring(L, "return function() end");
+
+	/* Teardown. */
+	lua_settop(L, 0);
+
+	return TEST_EXIT_SUCCESS;
+}
+
+#define TYPE_NAME "int"
+#define TEST_VALUE 100
+#define TOSTR(s) #s
+
+static int __call(lua_State *L)
+{
+	int *n = (int *)luaL_checkudata(L, 1, TYPE_NAME);
+	lua_pushfstring(L, "%d", *n);
+	return 1;
+}
+
+static const luaL_Reg mt[] = {
+	{ "__call", __call},
+	{ NULL, NULL}
+};
+
+static int bbb(lua_State *L) {
+	/* luaL_dostring(L, "local function varg_func(...) end return function() return varg_func() end"); */
+	luaL_dostring(L, "repeat until nil > 1");
+	return 0;
+}
+
+static int aaa(lua_State *L) {
+	(void)L;
+	luaL_dostring(L, "local function varg_func(...) end return function() return varg_func() end");
+	lua_pushcfunction(L, (lua_CFunction)bbb);
+	lua_call(L, 0, 0);
+	luaL_dostring(L, "local function varg_func(...) end return function() return varg_func() end");
+	return 1;
+}
+
+static int handle_c_frame(void *test_state)
+{
+	/* Setup. */
+	lua_State *L = test_state;
+	jit_attach(L, (void *)trace_cb, "trace");
+
+	/* Frame with a broken Lua code. */
+	luaL_dostring(L, "repeat until nil > 1");
+	/* luaL_dostring(L, "local function varg_func(...) end return function() return varg_func() end"); */
+
+	/* Frame with C function. */
+	/* lua_getfield(L, LUA_GLOBALSINDEX, "print"); */
+	/* lua_pcall(L, 0, 0, 0); */
+
+	/* lua_pushcfunction(L, (lua_CFunction)aaa); */
+	/* lua_pcall(L, 0, 0, 0); */
+
+	lua_pushcfunction(L, (lua_CFunction)bbb);
+	lua_pushcfunction(L, (lua_CFunction)aaa);
+	lua_pcall(L, 0, 0, -2);
+
+	/* Teardown. */
+	lua_settop(L, 0);
+
+	return TEST_EXIT_SUCCESS;
+}
+
+static int handle_cont_frame(void *test_state)
+{
+	/* Setup. */
+	lua_State *L = test_state;
+	jit_attach(L, (void *)trace_cb, "trace");
+
+	/* Frame with a broken Lua code. */
+	luaL_dostring(L, "repeat until nil > 1");
+
+	/* Frame with C function. */
+	luaL_newmetatable(L, TYPE_NAME);
+	luaL_register(L, 0, mt);
+	lua_pop(L, 1);
+	int *n = (int *)lua_newuserdata(L, sizeof(*n));
+	*n = TEST_VALUE;
+	luaL_getmetatable(L, TYPE_NAME);
+	lua_setmetatable(L, -2);
+	lua_pcall(L, 0, 1, 0);
+
+	const char *res = lua_tostring(L, -1);
+	assert_str_equal(res, TOSTR(TEST_VALUE));
+
+	/* Teardown. */
+	lua_settop(L, 0);
+
+	return TEST_EXIT_SUCCESS;
+}
+
+static int handle_bottom_frame(void *test_state)
 {
 	lua_State *L = test_state;
-	if (!L)
-		return TEST_EXIT_FAILURE;
-	luaL_openlibs(L);
 
 	/* Attach VM call handler. */
 	jit_attach(L, (void *)trace_cb, "trace");
 
-	luaL_dostring(L, "jit.opt.start('hotloop=1')");
-
 	/* Load a Lua code that generate a trace abort. */
-	luaL_dostring(L, "repeat until a >= 'b' > 'b'");
+	luaL_dostring(L, "repeat until nil > 1");
 
 	/* Triggers segmentation fault. */
 	jit_attach(L, (void *)trace_cb, NULL);
 
-	/* Make sure VM handler call was actually called. */
-	assert_true(is_cb_called == 1);
-
 	/* Clear Lua stack. */
-	lua_pop(L, 0);
+	lua_settop(L, 0);
 
 	return TEST_EXIT_SUCCESS;
 }
@@ -70,9 +166,15 @@ int main(void)
 	lua_State *L = utils_lua_init();
 	const struct test_unit tgroup[] = {
 #ifdef LJ_HASJIT
-		test_unit_def(call_vm_handler)
+		/* test_unit_def(handle_luafunc_frame), */
+		/* test_unit_def(handle_bottom_frame), */
+		/* test_unit_def(handle_cont_frame), */
+		test_unit_def(handle_c_frame),
 #endif /* LJ_HASJIT */
 	};
+	luaL_openlibs(L);
+	int res = luaL_dostring(L, "jit.opt.start('hotloop=1')");
+	assert(res == 0);
 	const int test_result = test_run_group(tgroup, L);
 	utils_lua_close(L);
 	return test_result;
