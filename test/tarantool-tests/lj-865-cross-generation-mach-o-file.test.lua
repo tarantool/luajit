223d4b5e4dff0e7c6c45ad182b5e432de86a790f
Mike Pall <mike>
2024-04-19 00:31:06 +0200

    OSX/iOS: Always generate 64 bit non-FAT Mach-O object files.
    Reported by Sergey Bronnikov.
    
    (cherry picked from commit 7110b935672489afd6ba3eef3e5139d2f3bd05b6)
    
    Previously, LuaJIT generated Mach-O FAT object files for ARM and
    ARM64 on macOS. The patch removes support of 32-bit Mach-O object
    files and FAT (Universal Binary) object files for OSX and now
    LuaJIT generate only 64-bit Mach-O object files.
    
    Sergey Bronnikov:
    * added the description and the trimmed the test for the problem
    
    Part of tarantool/tarantool#10199
    
    Reviewed-by: Sergey Kaplun <skaplun@tarantool.org>
    Reviewed-by: Maxim Kokryashkin <m.kokryashkin@tarantool.org>
    Signed-off-by: Sergey Kaplun <skaplun@tarantool.org>
    (cherry picked from commit 3b3c0936d7331dc817ccf89bc6e69769410cede3)

--- test/tarantool-tests/lj-865-cross-generation-mach-o-file.test.lua
+++ test/tarantool-tests/lj-865-cross-generation-mach-o-file.test.lua
@@ -3,8 +3,6 @@ local test = tap.test('lj-865-cross-generation-mach-o-file')
 local utils = require('utils')
 local ffi = require('ffi')
 
-test:plan(2)
-
 -- The test creates an object file in Mach-O format with LuaJIT
 -- bytecode and checks the validity of the object file fields.
 --
@@ -31,53 +29,14 @@ test:plan(2)
 -- $ file test.o
 -- empty.o: DOS executable (block device driver)
 
--- LuaJIT can generate so called Universal Binary with Lua
--- bytecode. The Universal Binary format is a format for
--- executable files that run natively on hardware platforms with
--- different hardware architectures. This concept is more
--- generally known as a fat binary.
---
 -- The format of the Mach-O is described in the document
 -- "OS X ABI Mach-O File Format Reference", published by Apple
 -- company. The copy of the (now removed) official documentation
--- can be found here [1]. Yet another source of truth is
--- XNU headers, see the definition of C-structures in:
--- [2] (`nlist_64`), [3] (`fat_arch` and `fat_header`).
+-- can be found here [1]. There is a good visual representation
+-- of Mach-O format in "Mac OS X Internals" book (pages 67-68)
+-- [2] and in the [3].
 --
--- There is a good visual representation of Universal Binary
--- in "Mac OS X Internals" book (pages 67-68) [5] and in the [6].
--- Below is the schematic structure of Universal Binary, which
--- includes two executables for PowerPC and Intel i386 (omitted):
---
---   0x0000000 ---------------------------------------
---             |
--- struct      | 0xcafebabe  FAT_MAGIC                 magic
--- fat_header  | -------------------------------------
---             | 0x00000003                            nfat_arch
---             ---------------------------------------
---             | 0x00000012  CPU_TYPE_POWERPC          cputype
---             | -------------------------------------
---             | 0x00000000  CPU_SUBTYPE_POWERPC_ALL   cpusubtype
--- struct      | -------------------------------------
--- fat_arch    | 0x00001000  4096 bytes                offset
---             | -------------------------------------
---             | 0x00004224  16932 bytes               size
---             | -------------------------------------
---             | 0x0000000c  2^12 = 4096 bytes         align
---             ---------------------------------------
---             ---------------------------------------
---             | 0x00000007  CPU_TYPE_I386             cputype
---             | -------------------------------------
---             | 0x00000003  CPU_SUBTYPE_I386_ALL      cpusubtype
--- struct      | -------------------------------------
--- fat_arch    | 0x00006000  24576 bytes               offset
---             | -------------------------------------
---             | 0x0000292c  10540 bytes               size
---             | -------------------------------------
---             | 0x0000000c  2^12 = 4096 bytes         align
---             ---------------------------------------
---               Unused
--- 0x00001000  ---------------------------------------
+-- 0x0000000  ---------------------------------------
 --             | 0xfeedface  MH_MAGIC                  magic
 --             | ------------------------------------
 --             | 0x00000012  CPU_TYPE_POWERPC          cputype
@@ -97,15 +56,22 @@ test:plan(2)
 --               Data
 --             ---------------------------------------
 --
---               < x86 executable >
---
 -- 1. https://github.com/aidansteele/osx-abi-macho-file-format-reference
--- 2. https://github.com/apple-oss-distributions/xnu/blob/xnu-10002.1.13/EXTERNAL_HEADERS/mach-o/nlist.h
--- 3. https://github.com/apple-oss-distributions/xnu/blob/xnu-10002.1.13/EXTERNAL_HEADERS/mach-o/fat.h
--- 4. https://developer.apple.com/documentation/apple-silicon/addressing-architectural-differences-in-your-macos-code
--- 5. https://reverseengineering.stackexchange.com/a/6357/46029
--- 6. http://formats.kaitai.io/mach_o/index.html
---
+-- 2. https://reverseengineering.stackexchange.com/a/6357/46029
+-- 3. http://formats.kaitai.io/mach_o/index.html
+
+test:plan(1)
+
+local CPU_SUBTYPE_ARM64 = 0x0
+local CPU_TYPE_ARM64 = '0x100000c'
+-- MH_MAGIC_64: mach-o, big-endian, x64.
+local MH_MAGIC_64 = '0xfeedfacf'
+-- Relocatable object file.
+local MH_OBJECT = 0x1
+
+-- The test creates an object file in Mach-O format with LuaJIT
+-- bytecode and checks the validity of the object file fields.
+
 -- Using the same declarations as defined in <src/jit/bcsave.lua>.
 ffi.cdef[[
 typedef struct
@@ -121,23 +87,9 @@ typedef struct
 typedef struct {
   uint32_t cmd, cmdsize;
   char segname[16];
-  uint32_t vmaddr, vmsize, fileoff, filesize;
-  uint32_t maxprot, initprot, nsects, flags;
-} mach_segment_command;
-
-typedef struct {
-  uint32_t cmd, cmdsize;
-  char segname[16];
   uint64_t vmaddr, vmsize, fileoff, filesize;
   uint32_t maxprot, initprot, nsects, flags;
 } mach_segment_command_64;
-
-typedef struct {
-  char sectname[16], segname[16];
-  uint32_t addr, size;
-  uint32_t offset, align, reloff, nreloc, flags;
-  uint32_t reserved1, reserved2;
-} mach_section;
 
 typedef struct {
   char sectname[16], segname[16];
@@ -153,52 +105,18 @@ typedef struct {
 typedef struct {
   int32_t strx;
   uint8_t type, sect;
-  int16_t desc;
-  uint32_t value;
-} mach_nlist;
-
-typedef struct {
-  int32_t strx;
-  uint8_t type, sect;
   uint16_t desc;
   uint64_t value;
 } mach_nlist_64;
 
-typedef struct
-{
-  int32_t magic, nfat_arch;
-} mach_fat_header;
-
-typedef struct
-{
-  int32_t cputype, cpusubtype, offset, size, align;
-} mach_fat_arch;
-
 typedef struct {
-  mach_fat_header fat;
-  mach_fat_arch fat_arch[2];
-  struct {
-    mach_header hdr;
-    mach_segment_command seg;
-    mach_section sec;
-    mach_symtab_command sym;
-  } arch[2];
-  mach_nlist sym_entry;
-  uint8_t space[4096];
-} mach_fat_obj;
-
-typedef struct {
-  mach_fat_header fat;
-  mach_fat_arch fat_arch[2];
-  struct {
-    mach_header_64 hdr;
-    mach_segment_command_64 seg;
-    mach_section_64 sec;
-    mach_symtab_command sym;
-  } arch[2];
+  mach_header_64 hdr;
+  mach_segment_command_64 seg;
+  mach_section_64 sec;
+  mach_symtab_command sym;
   mach_nlist_64 sym_entry;
   uint8_t space[4096];
-} mach_fat_obj_64;
+} mach_obj_64;
 ]]
 
 local function create_obj_file(name, arch)
@@ -212,108 +130,30 @@ local function create_obj_file(name, arch)
   return mach_o_path
 end
 
--- Parses a buffer in the Mach-O format and returns the FAT magic
--- number and `nfat_arch`.
-local function read_mach_o(buf, hw_arch)
-  local res = {
-    header = {
-      magic = 0,
-      nfat_arch = 0,
-    },
-    fat_arch = {},
-  }
+-- Parses a buffer in the Mach-O format and returns its fields
+-- in a table.
+local function read_mach_o_hdr(buf, hw_arch)
+  -- LuaJIT always generates 64-bit non-FAT Mach-O object files.
+  assert(hw_arch == 'arm64')
 
-  local is64 = hw_arch == 'arm64'
+  -- Mach-O object.
+  local mach_obj_type = ffi.typeof('mach_obj_64 *')
+  local obj = ffi.cast(mach_obj_type, buf)
 
-  -- Mach-O FAT object.
-  local mach_fat_obj_type = ffi.typeof(is64 and
-                                       'mach_fat_obj_64 *' or
-                                       'mach_fat_obj *')
-  local obj = ffi.cast(mach_fat_obj_type, buf)
+  -- Mach-O object header.
+  local mach_header = obj.hdr
 
-  -- Mach-O FAT object header.
-  local mach_fat_header = obj.fat
-  -- Mach-O FAT is BE, target arch is LE.
-  local be32 = bit.bswap
-  res.header.magic = be32(mach_fat_header.magic)
-  res.header.nfat_arch = be32(mach_fat_header.nfat_arch)
-
-  -- Mach-O FAT object arches.
-  for i = 0, res.header.nfat_arch - 1 do
-    local fat_arch = obj.fat_arch[i]
-    local arch = {
-      cputype = be32(fat_arch.cputype),
-      cpusubtype = be32(fat_arch.cpusubtype),
-    }
-    table.insert(res.fat_arch, arch)
-  end
-
-  return res
+  return mach_header
 end
 
--- Universal Binary can contain executables for more than one
--- CPU architecture. For simplicity, the test compares the *sum*
--- of CPU types and CPU subtypes.
---
--- <src/jit/bcsave.lua:bcsave_machobj> has the definitions of the
--- numbers below. The original XNU source code may be found in
--- <osfmk/mach/machine.h> [1].
---
--- 1. https://opensource.apple.com/source/xnu/xnu-4570.41.2/osfmk/mach/machine.h.auto.html
---
-local SUM_CPUTYPE = {
-  -- x86 + arm.
-  arm = 7 + 12,
-  -- x64 + arm64.
-  arm64 = 0x01000007 + 0x0100000c,
-}
-local SUM_CPUSUBTYPE = {
-  -- x86 + arm.
-  arm = 3 + 9,
-  -- x64 + arm64.
-  arm64 = 3 + 0,
-}
-
--- The function builds Mach-O FAT object file and retrieves
--- its header fields (magic and nfat_arch) and fields of each arch
--- (cputype, cpusubtype).
---
--- The Mach-O FAT object header can be retrieved with `otool` on
--- macOS:
---
--- $ otool -f empty.o
--- Fat headers
--- fat_magic 0xcafebabe
--- nfat_arch 2
--- <snipped>
---
--- CPU type and subtype can be retrieved with `lipo` on macOS:
---
--- $ luajit -b -o osx -a arm empty.lua empty.o
--- $ lipo -archs empty.o
--- i386 armv7
--- $ luajit -b -o osx -a arm64 empty.lua empty.o
--- $ lipo -archs empty.o
--- x86_64 arm64
+-- The function builds a Mach-O object file and validates its
+-- header fields.
 local function build_and_check_mach_o(subtest)
   local hw_arch = subtest.name
-  assert(hw_arch == 'arm' or hw_arch == 'arm64')
+  -- LuaJIT always generates 64-bit, non-FAT Mach-O object files.
+  assert(hw_arch == 'arm64')
 
-  subtest:plan(4)
-  -- FAT_MAGIC is an integer containing the value 0xCAFEBABE in
-  -- big-endian byte order format. On a big-endian host CPU,
-  -- this can be validated using the constant FAT_MAGIC;
-  -- on a little-endian host CPU, it can be validated using
-  -- the constant FAT_CIGAM.
-  --
-  -- FAT_NARCH is an integer specifying the number of fat_arch
-  -- data structures that follow. This is the number of
-  -- architectures contained in this binary.
-  --
-  -- See the aforementioned "OS X ABI Mach-O File Format
-  -- Reference".
-  local FAT_MAGIC = '0xffffffffcafebabe'
-  local FAT_NARCH = 2
+  subtest:plan(5)
 
   local MODULE_NAME = 'lango_team'
 
@@ -321,30 +161,21 @@ local function build_and_check_mach_o(subtest)
   local mach_o_buf = utils.tools.read_file(mach_o_obj_path)
   assert(mach_o_buf ~= nil and #mach_o_buf ~= 0, 'cannot read an object file')
 
-  local mach_o = read_mach_o(mach_o_buf, hw_arch)
+  local mach_o = read_mach_o_hdr(mach_o_buf, hw_arch)
 
   -- Teardown.
   assert(os.remove(mach_o_obj_path), 'remove an object file')
 
-  local magic_str = string.format('%#x', mach_o.header.magic)
-  subtest:is(magic_str, FAT_MAGIC,
-             'fat_magic is correct in Mach-O')
-  subtest:is(mach_o.header.nfat_arch, FAT_NARCH,
-             'nfat_arch is correct in Mach-O')
-
-  local total_cputype = 0
-  local total_cpusubtype = 0
-  for i = 1, FAT_NARCH do
-    total_cputype = total_cputype + mach_o.fat_arch[i].cputype
-    total_cpusubtype = total_cpusubtype + mach_o.fat_arch[i].cpusubtype
-  end
-  subtest:is(total_cputype, SUM_CPUTYPE[hw_arch],
-             'cputype is correct in Mach-O')
-  subtest:is(total_cpusubtype, SUM_CPUSUBTYPE[hw_arch],
+  local magic_str = string.format('%#x', mach_o.magic)
+  subtest:is(magic_str, MH_MAGIC_64, 'magic is correct in Mach-O')
+  local cputype_str = string.format('%#x', mach_o.cputype)
+  subtest:is(cputype_str, CPU_TYPE_ARM64, 'cputype is correct in Mach-O')
+  subtest:is(mach_o.cpusubtype, CPU_SUBTYPE_ARM64,
              'cpusubtype is correct in Mach-O')
+  subtest:is(mach_o.filetype, MH_OBJECT, 'filetype is correct in Mach-O')
+  subtest:is(mach_o.ncmds, 2, 'ncmds is correct in Mach-O')
 end
 
-test:test('arm', build_and_check_mach_o)
 test:test('arm64', build_and_check_mach_o)
 
 test:done(true)
