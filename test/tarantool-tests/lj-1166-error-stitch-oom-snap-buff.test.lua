6f215c1974e2039f52ac84864f421f24d407780a
Mike Pall <mike>
2024-04-18 23:57:53 +0200

    Use generic trace error for OOM during trace stitching.
    Thanks to Sergey Kaplun.
    
    (cherry picked from commit b8b49bf3954b23e32e34187a6ada00021c26e172)
    
    The previous commit doesn't handle the case when the error code is
    `LUA_ERRMEM`. This patch adds a workaround by using the generic error
    message.
    
    Sergey Kaplun:
    * added the description and the test for the problem
    
    Part of tarantool/tarantool#9924
    
    Reviewed-by: Maxim Kokryashkin <m.kokryashkin@tarantool.org>
    Reviewed-by: Sergey Bronnikov <sergeyb@tarantool.org>
    Signed-off-by: Sergey Kaplun <skaplun@tarantool.org>
    (cherry picked from commit 855798991d837070c658a788e4e3d3137a56211d)

--- test/tarantool-tests/lj-1166-error-stitch-oom-snap-buff.test.lua
+++ test/tarantool-tests/lj-1166-error-stitch-oom-snap-buff.test.lua
@@ -10,9 +10,15 @@ local test = tap.test('lj-1166-error-stitch-oom-snap-buff'):skipcond({
   ['Disabled on *BSD due to #4819'] = jit.os == 'BSD',
 })
 
-test:plan(1)
-
+local jparse = require('utils').jit.parse
 local allocinject = require('allocinject')
+
+-- XXX: Avoid other traces compilation due to hotcount collisions
+-- for predictable results.
+jit.off()
+jit.flush()
+
+test:plan(2)
 
 -- Generate the following Lua chunk:
 -- for i = 1, 2 do
@@ -34,6 +40,7 @@ local function create_chunk(n_conds)
   return chunk
 end
 
+jit.on()
 -- XXX: Need to compile the cycle in the `create_chunk()` to
 -- preallocate the snapshot buffer.
 jit.opt.start('hotloop=1', '-loop', '-fold')
@@ -45,6 +52,9 @@ local tracef = assert(loadstring(create_chunk(6)))
 jit.off()
 jit.flush()
 
+-- We only need the abort reason in the test.
+jparse.start('t')
+
 -- XXX: Update hotcounts to avoid hash collisions.
 jit.opt.start('hotloop=1')
 jit.on()
@@ -55,6 +65,27 @@ tracef()
 
 allocinject.disable()
 
+local _, aborted_traces = jparse.finish()
+
+jit.off()
+
 test:ok(true, 'stack is balanced')
+
+-- Tarantool may compile traces on the startup. These traces
+-- already exceed the maximum snapshot amount before the trace in
+-- this test is compiled. Hence, there is no need to reallocate
+-- the snapshot buffer, so the check for the snap size is not
+-- triggered.
+test:skipcond({
+  ['Impossible to predict the number of snapshots for Tarantool'] = _TARANTOOL,
+})
+
+assert(aborted_traces and aborted_traces[1], 'aborted trace is persisted')
+
+-- We tried to compile only one trace.
+local reason = aborted_traces[1][1].abort_reason
+
+test:like(reason, 'error thrown or hook called during recording',
+          'abort reason is correct')
 
 test:done(true)
