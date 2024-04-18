a7da08b2b15a0ef4afce4ee604dc02aa259ed8a8
Mike Pall <mike>
2024-04-19 01:33:19 +0200

    FFI: Turn FFI finalizer table into a proper GC root.
    Reported by Sergey Bronnikov.
    
    (cherry picked from commit f5affaa6c4e7524e661484f22f24255f9a83eb47)
    
    The previous patch fixes the problem partially because the introduced GC
    root may not exist at the start phase of the GC cycle (since it isn't
    marked because it is not accessible from any GC root). In that case, the
    cdata finalizer table will be collected at the end of the cycle. Access
    to the cdata finalizer table exhibits heap use after free. The patch
    turns the finalizer table into a proper GC root. Note that the finalizer
    table is created at the initialization of the main Lua State instead of
    loading the FFI library.
    
    Sergey Bronnikov:
    * added the description and the test for the problem
    
    Part of tarantool/tarantool#10199
    
    Reviewed-by: Sergey Kaplun <skaplun@tarantool.org>
    Reviewed-by: Maxim Kokryashkin <m.kokryashkin@tarantool.org>
    Signed-off-by: Sergey Kaplun <skaplun@tarantool.org>
    (cherry picked from commit 4b0f1724cab405b90c668134d42c6089f25e45e1)

--- test/tarantool-c-tests/lj-1168-unmarked-finalizer-tab.test.c
+++ test/tarantool-c-tests/lj-1168-unmarked-finalizer-tab.test.c
@@ -65,10 +65,57 @@ static int unmarked_finalizer_tab_gcstart(void *test_state)
 	return TEST_EXIT_SUCCESS;
 }
 
+static int
+unmarked_finalizer_tab_gcmark(void *test_state)
+{
+	/* Shared Lua state is not needed. */
+	UNUSED(test_state);
+
+	/* Setup. */
+	lua_State *L = luaL_newstate();
+
+	/* Set GC at the start. */
+	lua_gc(L, LUA_GCCOLLECT, 0);
+
+	/*
+	 * Default step is too big -- one step ends after the
+	 * atomic phase.
+	 */
+	lua_gc(L, LUA_GCSETSTEPMUL, 1);
+
+	/* Skip marking roots. */
+	lua_gc(L, LUA_GCSTEP, 1);
+
+	/* Not trigger GC during `lua_openffi()`. */
+	lua_gc(L, LUA_GCSTOP, 0);
+
+	/*
+	 * The terminating '\0' is considered by parser as part of
+	 * the input, so we must chomp it.
+	 */
+	int res = luaL_loadbufferx(L, buff, sizeof(buff) - 1,
+				   "test_chunk", "t");
+	if (res != LUA_OK) {
+		test_comment("error loading Lua chunk: %s",
+			     lua_tostring(L, -1));
+		bail_out("error loading Lua chunk");
+	}
+
+	/* Finish GC cycle to collect the finalizer table. */
+	while (!lua_gc(L, LUA_GCSTEP, -1));
+
+	/* Teardown. */
+	lua_settop(L, 0);
+	lua_close(L);
+
+	return TEST_EXIT_SUCCESS;
+}
+
 int main(void)
 {
 	const struct test_unit tgroup[] = {
 		test_unit_def(unmarked_finalizer_tab_gcstart),
+		test_unit_def(unmarked_finalizer_tab_gcmark),
 	};
 	const int test_result = test_run_group(tgroup, NULL);
 
