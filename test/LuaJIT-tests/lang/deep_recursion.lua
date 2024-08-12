e390d8b21b7451c6e144918f6fafb1f83fc460fe
Sergey Kaplun <skaplun@tarantool.org>
2024-08-12 10:38:57 +0300

    test: enable <misc/recurse_deep.lua> LuaJIT test
    This patch moves the aforementioned test from the <misc> to the <lang/>
    directory (with slightly renaming to be consistent with other names),
    includes it in <index>, and names the subtests.
    
    Part of tarantool/tarantool#9398
    
    Reviewed-by: Sergey Bronnikov <sergeyb@tarantool.org>
    Reviewed-by: Maxim Kokryashkin <m.kokryashkin@tarantool.org>
    Signed-off-by: Sergey Kaplun <skaplun@tarantool.org>
    (cherry picked from commit 6b970ae9fcd47b1d4d75909dd60257638107e91b)

--- test/LuaJIT-tests/lang/deep_recursion.lua
+++ test/LuaJIT-tests/lang/deep_recursion.lua
@@ -1,5 +1,4 @@
-
-do
+do --- Recursion sum.
   local function sum(n)
     if n == 1 then return 1 end
     return n + sum(n-1)
@@ -7,7 +6,7 @@ do
   assert(sum(200) == 20100)
 end
 
-do
+do --- Recursion wiht pcall.
   local pcall = pcall
   local tr1
   local x = 0
@@ -19,11 +18,10 @@ do
   assert(tr1(200) == true and x == 200)
 end
 
-do
+do --- Recursion fibonacci.
   local function fib(n)
     if n < 2 then return 1 end
     return fib(n-2) + fib(n-1)
   end
   assert(fib(15) == 987)
 end
-
