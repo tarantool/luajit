abcf9f2ff50f336bb5c852170e8720fbff9ddb0e
Mike Pall <mike>
2024-08-15 00:07:34 +0200

    Limit CSE for IR_CARG to fix loop optimizations.
    Thanks to Peter Cawley.
    
    (cherry picked from commit 3bdc6498c4c012a8fbf9cfa2756a5b07f56f1540)
    
    `IR_CALLXS` for the vararg function contains `IR_CARG(fptr, ctid)` as
    the second operand. The `loop_emit_phi()` scans only the first operand
    of the IR, so the second is not marked as PHI. In this case, when the IR
    appears in both the invariant and variant parts of the loop, CSE may
    remove it and thus lead to incorrect emitting results.
    
    This patch tweaks the CSE rules to avoid CSE across the `IR_LOOP`.
    
    Sergey Kaplun:
    * added the description and the test for the problem
    
    Part of tarantool/tarantool#10199
    
    Reviewed-by: Sergey Bronnikov <sergeyb@tarantool.org>
    Reviewed-by: Maxim Kokryashkin <m.kokryashkin@tarantool.org>
    Signed-off-by: Sergey Kaplun <skaplun@tarantool.org>
    (cherry picked from commit b52fe9795db249e7803aeef22c2ed6129a97aaeb)

--- src/lj_opt_fold.c
+++ src/lj_opt_fold.c
@@ -2406,6 +2406,17 @@ LJFOLD(XSNEW any any)
 LJFOLD(BUFHDR any any)
 LJFOLDX(lj_ir_emit)
 
+/* -- Miscellaneous ------------------------------------------------------- */
+
+LJFOLD(CARG any any)
+LJFOLDF(cse_carg)
+{
+  TRef tr = lj_opt_cse(J);
+  if (tref_ref(tr) < J->chain[IR_LOOP])  /* CSE across loop? */
+    return EMITFOLD;  /* Raw emit. Assumes fins is left intact by CSE. */
+  return tr;
+}
+
 /* ------------------------------------------------------------------------ */
 
 /* Every entry in the generated hash table is a 32 bit pattern:
