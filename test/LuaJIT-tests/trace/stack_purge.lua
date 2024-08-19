16f3d14e7f87c36660a3a165d1923ebd51867b95
Sergey Kaplun <skaplun@tarantool.org>
2024-08-19 10:50:31 +0300

    test: refactor <trace/stack_purge.lua> LuaJIT test
    This patch refactors the aforementioned test to make its code style
    closer to ours.
    
    Relates to tarantool/tarantool#9398
    
    Reviewed-by: Sergey Bronnikov <sergeyb@tarantool.org>
    Reviewed-by: Maxim Kokryashkin <m.kokryashkin@tarantool.org>
    Signed-off-by: Sergey Kaplun <skaplun@tarantool.org>

--- test/LuaJIT-tests/trace/stack_purge.lua
+++ test/LuaJIT-tests/trace/stack_purge.lua
@@ -11,11 +11,11 @@ local function b()
 end
 
 local function c()
-  for j=1,10 do
-    for i=1,50 do b() b() b() end
+  for _ = 1, 10 do
+    for _ = 1, 50 do b() b() b() end
     collectgarbage()
     local t = {}
-    for i=1,50 do t = {t} end
+    for _ = 1, 50 do t = {t} end
   end
 end
 
