7035dc49bf286655b252cb305ae3e282be644f32
Mike Pall <mike>
2024-08-19 17:31:15 +0200

    Fix another potential file descriptor leak in luaL_loadfile*().
    Reported by Peter Cawley.
    
    (cherry picked from commit ab39082fddfca0de268a106a3b6d736eef032328)
    
    `loadfile()` doesn't close the fd in case when `fopen()` results
    successfully, but then the file can't be read (since it is a directory,
    for example) or parser failure occurs for some reason.
    
    This patch fixes that behaviour by moving the error formatting after the
    cleanup of the descriptor.
    
    Sergey Kaplun:
    * added the description and the test for the problem
    
    Part of tarantool/tarantool#11278
    
    Reviewed-by: Sergey Bronnikov <sergeyb@tarantool.org>
    Signed-off-by: Sergey Kaplun <skaplun@tarantool.org>

--- test/tarantool-tests/lj-1249-loadfile-fd-leak.test.lua
+++ test/tarantool-tests/lj-1249-loadfile-fd-leak.test.lua
@@ -6,7 +6,7 @@ local tap = require('tap')
 -- See also, https://github.com/LuaJIT/LuaJIT/issues/1249.
 local test = tap.test('lj-1249-loadfile-fd-leak')
 
-test:plan(2)
+test:plan(4)
 
 local allocinject = require('allocinject')
 
@@ -23,5 +23,37 @@ allocinject.disable()
 test:ok(not res, 'correct status, OOM on filename creation')
 test:like(errmsg, 'not enough memory',
           'correct error message, OOM on filename creation')
+
+-- Now try to read the directory. It can be opened but not read as
+-- a file on Linux-like systems.
+
+-- On macOS and BSD-like systems, the content of the directory may
+-- be read and contain some internal data, which we are not
+-- interested in.
+test:skipcond({
+  ['Disabled on non-Linux systems'] = jit.os ~= 'Linux',
+})
+
+local DIRNAME = '/dev'
+local GCSTR_OBJSIZE = 24
+
+-- Now the OOM error should be obtained on the creation of the
+-- error message that the given file (or directory) is not
+-- readable. But the string for the file name should be allocated
+-- without OOM, so set the corresponding limit for the string
+-- object.
+-- Don't forget to count the leading '@' and trailing '\0'.
+allocinject.enable_null_limited_alloc(#DIRNAME + GCSTR_OBJSIZE + 1 + 1)
+
+-- Error since can't read the directory (but actually the OOM on
+-- parsing preparation is raised before, due to allocation limit).
+res, errmsg = pcall(loadfile, DIRNAME)
+
+allocinject.disable()
+
+-- Sanity checks.
+test:ok(not res, 'correct status, OOM on error message creation')
+test:like(errmsg, 'not enough memory',
+          'correct error message, OOM on error message creation')
 
 test:done(true)
