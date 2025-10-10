5fd744fca5f9246506ad8357c02ef2a3828d3826
Sergey Kaplun <skaplun@tarantool.org>
2025-10-10 14:45:04 +0300

    perf: adjust chameneos in LuaJIT-benches
    This patch adjusts the aforementioned test to use the benchmark
    framework introduced before. The default arguments are adjusted
    according to the <PARAM_x86.txt> file. The arguments to the script still
    can be provided in the command line run.
    
    Reviewed-by: Sergey Bronnikov <sergeyb@tarantool.org>
    Signed-off-by: Sergey Kaplun <skaplun@tarantool.org>

--- perf/LuaJIT-benches/chameneos.lua
+++ perf/LuaJIT-benches/chameneos.lua
@@ -1,8 +1,16 @@
+-- The benchmark to check the performance of coroutine interaction
+-- using symmetrical rendezvous requests.
+-- For the details see:
+-- https://pybenchmarks.org/u64q/performance.php?test=chameneosredux
+-- https://cedric.cnam.fr/PUBLIS/RC474.pdf
+
+local bench = require("bench").new(arg)
 
 local co = coroutine
 local create, resume, yield = co.create, co.resume, co.yield
 
-local N = tonumber(arg and arg[1]) or 10
+local N = tonumber(arg and arg[1]) or 1e7
+local N_ATTEMPTS = N
 local first, second
 
 -- Meet another creature.
@@ -57,12 +65,24 @@ local function schedule(threads)
   until false
 end
 
--- A bunch of colorful creatures.
-local threads = {
-  creature("blue"),
-  creature("red"),
-  creature("yellow"),
-  creature("blue"),
-}
+bench:add({
+  name = "chameneos",
+  items = N_ATTEMPTS,
+  checker = function(meetings) return meetings == N_ATTEMPTS * 2 end,
+  payload = function()
+    -- A bunch of colorful creatures.
+    local threads = {
+      creature("blue"),
+      creature("red"),
+      creature("yellow"),
+      creature("blue"),
+    }
 
-io.write(schedule(threads), "\n")
+    local meetings = schedule(threads)
+    -- XXX: Restore meetings for the next iteration.
+    N = N_ATTEMPTS
+    return meetings
+  end,
+})
+
+bench:run_and_report()
